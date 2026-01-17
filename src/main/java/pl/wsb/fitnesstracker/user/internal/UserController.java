2ee8d9a7eff0ca99e30293e156949ffb04915f86
filipwesolowski <filipwesolowski09@gmail.com>
2026-01-17 16:43:13 +0100

    lab 6

--- src/main/java/pl/wsb/fitnesstracker/user/internal/UserController.java
+++ src/main/java/pl/wsb/fitnesstracker/user/internal/UserController.java
@@ -1,9 +1,12 @@
 package pl.wsb.fitnesstracker.user.internal;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import pl.wsb.fitnesstracker.user.api.UserDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import pl.wsb.fitnesstracker.user.api.*;
 
 import java.net.URI;
 import java.time.LocalDate;
@@ -15,16 +18,12 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/v1/users")
+@RequiredArgsConstructor
 class UserController {
 
     private final UserServiceImpl userService;
 
     private final UserMapper userMapper;
-
-    public UserController(UserServiceImpl userService, UserMapper userMapper) {
-        this.userService = userService;
-        this.userMapper = userMapper;
-    }
 
     @GetMapping
     public List<UserDto> getAllUsers() {
